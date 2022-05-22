d3483ebd127a0c8ef7d005752cd5a5d5f5cc410e
batram <ym@batr.am>
2022-05-22 09:33:00 +0200

    Always save gameObject.name in overrideName
    - store and restore the name in snapshot XML
    - this will be used to info about layers and maybe other things

--- Patches/RestoreSaveablesPatch.cs
+++ Patches/RestoreSaveablesPatch.cs
@@ -15,6 +15,22 @@ namespace BackgroundBlocks.Patches
                 if (saveable.placeable && saveable.blockID >= BackgroundBlocksMod.magicBackgroundNumber)
                 {
                     BackgroundBlocksMod.EnableBackground(saveable.placeable.gameObject);
+                    saveable.overrideName = saveable.placeable.gameObject.name;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(QuickSaver), nameof(QuickSaver.GetSaveablesFromMetadata))]
+    static class GetSaveablesFromMetadataPatch
+    {
+        static void Postfix(ref List<QuickSaver.SaveablePiece> __result)
+        {
+            foreach (QuickSaver.SaveablePiece saveable in __result)
+            {
+                if (saveable.placeable && saveable.blockID >= BackgroundBlocksMod.magicBackgroundNumber)
+                {
+                    saveable.overrideName = saveable.placeable.gameObject.name;
                 }
             }
         }
