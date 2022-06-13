eb14522717a6085c9b834626fba9ef5ecffda2b9
batram <ym@batr.am>
2022-06-13 08:10:51 +0200

    CustomBlocks: RemoteControl - Skinned Punchblock and Honey

---
+++ CustomBlocks/RemoteControl/RCReceiver.cs
@@ -0,0 +1,208 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModBlocks.CustomBlocks
+{
+    class RCReceiver : CustomBlock
+    {
+        public const int XCustomId = 105;
+        public override int CustomId { get { return XCustomId; } }
+        public override int BasedId { get { return 32; } }
+
+        //GluePiece_SetPiece
+        public override string BasePlaceableName { get { return "Glue"; } }
+        public override string BasePickableBlockName { get { return "Glue_Pick"; } }
+
+        new public string Name = typeof(RCReceiver).Name;
+
+        protected Sprite sp;
+        new public Sprite sprite
+        {
+            get
+            {
+                if (sp == null)
+                {
+                    Texture2D texture = LoadTexture(Path.Combine(CustomBlock.ImageDir, this.Name + ".png"));
+                    sp = Sprite.Create(texture, new Rect(0, 0, 72, 73), new Vector2(0, 0), 100f);
+                    Object.DontDestroyOnLoad(sp);
+                }
+                return sp;
+            }
+        }
+
+        protected Sprite ind_sp;
+        public Sprite ind_sprite
+        {
+            get
+            {
+                if (ind_sp == null)
+                {
+                    Texture2D texture = LoadTexture(Path.Combine(CustomBlock.ImageDir, this.Name + "_selection_indicator.png"));
+                    ind_sp = Sprite.Create(texture, new Rect(0, 0, 72, 73), new Vector2(0, 0), 100f);
+                    Object.DontDestroyOnLoad(ind_sp);
+                }
+                return ind_sp;
+            }
+        }
+
+        public Placeable AttachedTo;
+        public float og_interval;
+        public Placeable ConnectedTransmitter;
+        public SpriteRenderer Indicator_spr;
+        public Placeable Honey;
+
+
+        public RCReceiver()
+        {
+            base.Name = this.Name;
+        }
+
+        override public PickableBlock CreatePickableBlock()
+        {
+            PickableBlock pb = base.CreatePickableBlock();
+            pb.transform.localPosition -= new Vector3(19f, 26.5f, 1);
+            pb.transform.localScale = new Vector3(0.75f, 0.75f, 1);
+
+            var BaseSprite = pb.transform.Find("ArtHolder/rotatingCenter/BaseSprite");
+
+            BaseSprite.GetComponent<SpriteRenderer>().sprite = sprite;
+            BaseSprite.transform.localPosition = new Vector3(-0.68f, -1.13f, 0);
+            BaseSprite.transform.localScale = new Vector3(1.4f, 1.4f, 1);
+
+            return pb;
+        }
+
+        override public Placeable CreatePlaceablePrefab()
+        {
+            Placeable placeable = base.CreatePlaceablePrefab();
+            placeable.gameObject.AddComponent<RCReceiver>();
+
+
+            var BaseSprite = placeable.transform.Find("GluePiece/Sprite");
+
+            BaseSprite.GetComponent<SpriteRenderer>().sprite = sprite;
+            BaseSprite.transform.localScale = new Vector3(1f, 1f, 1);
+            BaseSprite.transform.localPosition = new Vector3(-0.68f, - 1.13f, 0);
+
+
+            var Indicator = Object.Instantiate(BaseSprite, BaseSprite.transform.parent);
+            Indicator.name = "Indicator";
+            Indicator.transform.parent = BaseSprite.transform.parent;
+            this.Indicator_spr = Indicator.GetComponent<SpriteRenderer>();
+            //TODO: Fix Layer
+            //this.Indicator_spr.sortingLayerName = glove.GetComponent<SpriteRenderer>().sortingLayerName;
+            this.Indicator_spr.sprite = this.ind_sprite;
+            Debug.Log("RCR Place this.Indicator_spr init " + this.Indicator_spr);
+            this.Honey = placeable.transform.Find("GluePiece").GetComponent<HoneyPiece>();
+            Placeable.AllPlaceables = new List<Placeable> { };
+
+            return placeable;
+        }
+
+        override public void FixSprite(Transform sprite_holder)
+        {
+        }
+
+        override public void FixedUpdate()
+        {
+            if(ConnectedTransmitter == null)
+            {
+                ResetConnectionColor();
+            }
+        }
+
+
+        public void SetConnectionColor(Color c)
+        {
+            if (this.Indicator_spr)
+            {
+                this.Indicator_spr.material.color = c;
+            }
+        }
+
+        public void ResetConnectionColor()
+        {
+            if (this.Indicator_spr)
+            {
+                this.Indicator_spr.material.color = new Color(0, 0, 0, 1);
+            }
+        }
+
+        public void Trigger()
+        {
+            if (this.AttachedTo)
+            {
+                this.AttachedTo.transform.GetComponent<ProjectileLauncher>()?.ShootProjectile();
+                foreach (ProjectileLauncher pl in this.AttachedTo.transform.GetComponentsInChildren<ProjectileLauncher>())
+                {
+                    pl.ShootProjectile();
+                }
+                if (AttachedTo.GetType() == typeof(UpBlower))
+                {
+                    ToggleUpBlower((UpBlower)AttachedTo);
+                }
+            }
+        }
+
+        public void ToggleUpBlower(UpBlower blower)
+        {
+            var anim = blower.GetComponent<Animator>();
+            string[] comps = new string[] { "SolidCollider (1)", "BlowTriggerPlayer", "BlowTriggerProjectiles", "Particle System", "Particle System (1)", "Particle System (2)" };
+
+            if (anim.speed == 1)
+            {
+                anim.speed = 0;
+                foreach(string comp in comps)
+                {
+                    blower.transform.Find(comp).gameObject.SetActive(false);
+                }
+            } else
+            {
+                anim.speed = 1;
+                foreach (string comp in comps)
+                {
+                    blower.transform.Find(comp).gameObject.SetActive(true);
+                }
+            }
+        }
+
+        public override void OnPlace(Placeable placeable, int playerNumber, bool sendEvent, bool force = false)
+        {
+            Debug.Log("RCR Place " + placeable);
+            this.Indicator_spr = placeable.transform.Find("Indicator")?.GetComponent<SpriteRenderer>();
+            this.AttachedTo = placeable.transform.parent?.GetComponent<Placeable>();
+            Debug.Log("RCR Place AttachedTo " + this.AttachedTo);
+            if (this.AttachedTo)
+            {
+                if (AttachedTo.GetType() == typeof(ProjectileLauncher))
+                {
+                    og_interval = (AttachedTo as ProjectileLauncher).interval;
+                    (AttachedTo as ProjectileLauncher).interval = 1000000000;
+                } else if(AttachedTo.GetType() == typeof(UpBlower))
+                {
+
+                }
+            }
+        }
+        public override void OnDestroy()
+        {
+            if (this.AttachedTo)
+            {
+                if (AttachedTo.GetType() == typeof(ProjectileLauncher))
+                {
+                    (AttachedTo as ProjectileLauncher).interval = og_interval;
+                }
+                else if (AttachedTo.GetType() == typeof(UpBlower))
+                {
+                    UpBlower blower = (UpBlower) AttachedTo;
+                    blower.GetComponent<Animator>().speed = 1;
+                }
+            }
+            base.OnDestroy();
+
+        }
+    }
+}
---
+++ CustomBlocks/RemoteControl/RCTransmitter.cs
@@ -0,0 +1,199 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModBlocks.CustomBlocks
+{
+    class RCTransmitter : CustomBlock
+    {
+        public const int XCustomId = 106;
+        public override int CustomId { get { return XCustomId; } }
+        public override int BasedId { get { return 72; } }
+
+        public override string BasePlaceableName { get { return "BoxingGlove"; } }
+        public override string BasePickableBlockName { get { return "BoxingGlove_Pick"; } }
+
+        new public string Name = typeof(RCTransmitter).Name;
+
+        protected Sprite sp;
+        new public Sprite sprite
+        {
+            get
+            {
+                if (sp == null)
+                {
+                    Texture2D texture = LoadTexture(Path.Combine(CustomBlock.ImageDir, this.Name + ".png"));
+                    sp = Sprite.Create(texture, new Rect(0, 0, 248, 127), new Vector2(0, 0), 100f);
+                    Object.DontDestroyOnLoad(sp);
+                }
+                return sp;
+            }
+        }
+
+        protected Sprite ind_sp;
+        public Sprite ind_sprite
+        {
+            get
+            {
+                if (ind_sp == null)
+                {
+                    Texture2D texture = LoadTexture(Path.Combine(CustomBlock.ImageDir, this.Name + "_selection_indicator.png"));
+                    ind_sp = Sprite.Create(texture, new Rect(0, 0, 248, 127), new Vector2(0, 0), 100f);
+                    Object.DontDestroyOnLoad(ind_sp);
+                }
+                return ind_sp;
+            }
+        }
+
+        public RCReceiver ConnectedReceiver;
+        public SpriteRenderer Indicator_spr;
+
+
+        public RCTransmitter()
+        {
+            base.Name = this.Name;
+        }
+
+        override public PickableBlock CreatePickableBlock()
+        {
+            PickableBlock pb = base.CreatePickableBlock();
+            pb.transform.localPosition -= new Vector3(1f, 20.5f, 1);
+            pb.transform.localScale = new Vector3(0.75f, 0.75f, 1);
+
+            var sprite_holder = pb.transform.Find("SpriteHolder");
+            sprite_holder.GetComponent<Animator>().enabled = false ;
+            var glove = pb.transform.Find("SpriteHolder/Offset/Glove");
+
+            glove.GetComponent<SpriteRenderer>().sprite = sprite;
+            glove.transform.localScale = new Vector3(1.4f, 1.4f, 1);
+            sprite_holder.transform.localPosition = new Vector3(-0.53f, -0.53f, 0);
+            glove.transform.localPosition = new Vector3(-11.9327f, -1.25f, 0);
+
+            pb.gameObject.transform.Rotate(0, 0, 90f, Space.Self);
+
+            return pb;
+        }
+
+        override public Placeable CreatePlaceablePrefab()
+        {
+            Placeable placeable = base.CreatePlaceablePrefab();
+            placeable.gameObject.AddComponent<RCTransmitter>();
+
+
+            var sprite_holder = placeable.transform.Find("SpriteHolder/Offset/Glove");
+            //TODO: Custom animation
+            sprite_holder.GetComponent<Animator>().enabled = false;
+            var glove = placeable.transform.Find("SpriteHolder/Offset/Glove/Glove");
+
+            glove.GetComponent<SpriteRenderer>().sprite = sprite;
+            glove.transform.localScale = new Vector3(1.4f, 1.4f, 1);
+            sprite_holder.transform.localPosition = new Vector3(-1.68f, -0.63f, 0);
+
+            placeable.transform.Find("SpriteHolder/Indicators-Crossbow_indicator.000 (1)").gameObject.SetActive(false);
+
+            var Indicator = Object.Instantiate(glove, glove.transform.parent);
+            GameObject.DontDestroyOnLoad(Indicator);
+            Indicator.name = "Indicator";
+            Indicator.transform.parent = glove.transform.parent;
+            this.Indicator_spr = Indicator.GetComponent<SpriteRenderer>();
+            //TODO: Fix Layer
+            this.Indicator_spr.sortingLayerName = "MovingBlocks";
+            this.Indicator_spr.sprite = ind_sprite;
+            placeable.gameObject.transform.Rotate(0, 0, 90f, Space.Self);
+
+
+            return placeable;
+        }
+
+        override public void FixSprite(Transform sprite_holder)
+        {
+        }
+
+        public override void FixedUpdate()
+        {
+            if (this.gameObject.GetComponent<Placeable>().Placed)
+            {
+                ConnectToReceiver();
+            }
+            if (ConnectedReceiver == null)
+            {
+                ResetConnectionColor();
+            }
+        }
+
+        public void SetConnectionColor(Color c)
+        {
+            this.transform.Find("SpriteHolder/Offset/Glove/Indicator").GetComponent<SpriteRenderer>().material.color = c;
+        }
+
+        public void ResetConnectionColor()
+        {
+            this.transform.Find("SpriteHolder/Offset/Glove/Indicator").GetComponent<SpriteRenderer>().material.color = new Color(0, 0, 0, 1);
+        }
+
+
+        public override void OnPlace(Placeable __instance, int playerNumber, bool sendEvent, bool force = false)
+        {
+            Debug.Log("RCT Place " + playerNumber);
+
+        }
+
+        public void ConnectToReceiver()
+        {
+            //if (this.NetSurrogate == null || this.NetSurrogate.hasAuthority)
+            {
+                if (this.ConnectedReceiver != null)
+                {
+                    return;
+                }
+
+                foreach (RCReceiver receiver in GameObject.FindObjectsOfType<RCReceiver>())
+                {
+                    if (receiver != null && receiver.ConnectedTransmitter == null && receiver.gameObject.GetComponent<Placeable>().Placed)
+                    {
+                        this.ConnectedReceiver = receiver;
+                        receiver.ConnectedTransmitter = this;
+
+                        //TODO: Use preset of colors and sync connection color via network
+                        Color color = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+                        Debug.Log("Place color " + color);
+                        this.SetConnectionColor(color);
+                        receiver.SetConnectionColor(color);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PunchingBlock), nameof(PunchingBlock.Punch))]
+    static class PunchingBlockPatch
+    {
+        static void Postfix(PunchingBlock __instance, UnityEngine.Collider2D __0)
+        {
+            try
+            {
+                RCTransmitter rct = __instance.gameObject.GetComponent<RCTransmitter>();
+                if (rct && rct.ConnectedReceiver && rct.ConnectedReceiver.AttachedTo)
+                {
+                    rct.ConnectedReceiver.Trigger();
+                    __instance.StartCoroutine(Reset(__instance));
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogWarning($"Exception in patch :\n{ex}");
+            }
+        }
+
+        static public IEnumerator<WaitForSeconds> Reset(PunchingBlock __instance)
+        {
+            yield return new WaitForSeconds(0.3f);
+            __instance.punching = false;
+            yield return null;
+        }
+    }
+
+}
