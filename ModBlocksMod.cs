5bb0cd64fe659ad2adb3ef1743e9b92a3904c9b2
batram <ym@batr.am>
2022-07-13 12:53:10 +0200

    Change to version 0.2

--- ModBlocksMod.cs
+++ ModBlocksMod.cs
@@ -3,12 +3,12 @@ using HarmonyLib;
 using UnityEngine;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyInformationalVersion("0.2")]
 
 namespace ModBlocks
 {
-    [BepInPlugin("ModBlocks", "ModBlocks", "0.0.0.1")]
+    [BepInPlugin("ModBlocks", "ModBlocks", "0.2")]
     public class ModBlocksMod : BaseUnityPlugin
     {
         public static bool enableModBlockMode = false;
